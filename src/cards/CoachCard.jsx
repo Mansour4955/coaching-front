d3be1230f70c96387b625fa92f6cae1c5dd021ac
Amine Mansour <mansour4955amine@gmail.com>
2024-05-14 03:02:54 +0200

    update follow

--- src/cards/CoachCard.jsx
+++ src/cards/CoachCard.jsx
@@ -1,8 +1,60 @@
 import React, { useState } from "react";
 import useGetImages from "../hooks/useGetImages";
+import { useLocalStorage } from "../hooks/useLocalStorege";
+import axios from "axios";
+import { URL } from "../data";
 
-const CoachCard = ({ full_name, description, profilePhoto, domaine, id }) => {
+const CoachCard = ({
+  full_name,
+  description,
+  profilePhoto,
+  domaine,
+  id: coachProfileId,
+}) => {
+  const [catchError, setCatchError] = useState(null);
   const [showMore, setShowMore] = useState(false);
+  const { getItem } = useLocalStorage("userData");
+  const reviewer = getItem();
+  const handleFollow = () => {
+    console.log("Follow");
+    axios
+      .put(`${URL}/api/users/${coachProfileId}`, {
+        follow: [reviewer?._id],
+      })
+      .then((response) => {})
+      .catch((error) => {
+        setCatchError(error);
+        console.log("Error following coach ", error.response);
+      });
+    axios
+      .put(`${URL}/api/users/${reviewer?._id}`, {
+        following: [coachProfileId],
+      })
+      .then((response) => {})
+      .catch((error) => {
+        setCatchError(error);
+        console.log("Error following coach ", error.response);
+      });
+
+    if (!catchError) {
+      axios
+        .put(`${URL}/api/users/${coachProfileId}`, {
+          coachNotifications: [
+            {
+              user: reviewer?._id,
+              date: new Date().toISOString(),
+              action: "follow",
+            },
+          ],
+        })
+        .then((response) => {})
+        .catch((error) => {
+          console.log("Error following coach ", error.response);
+        });
+    } else {
+      console.log("can not put them in coach notifications");
+    }
+  };
 
   const hide = "overflow-hidden line-clamp-1";
   const imageOfCoach = useGetImages(profilePhoto);
@@ -27,7 +79,10 @@ const CoachCard = ({ full_name, description, profilePhoto, domaine, id }) => {
             >
               {showMore ? "See less" : "See more"}
             </button>
-            <button className="flex items-center justify-center px-2 border border-main_color rounded-xl font-semibold text-main_color duration-300 hover:bg-main_color hover:text-white">
+            <button
+              onClick={handleFollow}
+              className="flex items-center justify-center px-2 border border-main_color rounded-xl font-semibold text-main_color duration-300 hover:bg-main_color hover:text-white"
+            >
               <span className="text-xl">+</span> Follow
             </button>
           </div>
