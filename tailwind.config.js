274e71be6bcade29e1d20ab030b1fe670118c8b4
Mansour Amine <149618255+Mansour4955@users.noreply.github.com>
2024-12-01 21:36:20 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        main_color: '#fe300b',
+        main_color: '#26619c',
+        // main_color: '#fe300b',
         // main_color: "#f47628",
         white_color: "#dddddd",
         // white_color: '#f4f8ff',
